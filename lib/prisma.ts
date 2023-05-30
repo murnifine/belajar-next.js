b8fa7af317e3abaab5d95ba8ea6881bed6c25e65
Murnifine <murnifine@gmail.com>
2023-05-31 03:22:36 +0800

    perbaru kode lib prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,22 +2,16 @@ import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = globalWithPrisma.prisma;
 }
-
-
-// export async function handleComplete(id: number, isComplete: boolean) {
-//   await prisma.todo.update({
-//     where: { id },
-//     data: { isComplete: !isComplete },
-//   });
-// }
-
 
 export default prisma;
